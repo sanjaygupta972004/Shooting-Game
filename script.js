918f58de513947094104054ae432842a4001d213
Sanjay Gupta <sanjaygupta07054@gmail.com>
2023-08-03 00:50:54 +0530

    completed this project

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-﻿//==---basic Enviroment Setup==----
+﻿// importing sound effect
+const shootingsong= new Audio("./Music/shooting.mp3");
+const killenemy= new Audio("./Music/killEnemy.mp3");
+const gameover=new Audio("./Music/gameOver.mp3");
+const hugeweapon= new Audio("./Music/hugeWeapon.mp3");
+const heavyweapon= new Audio("./Music/heavyWeapon.mp3");
+//==---basic Enviroment Setup==----
 const canvas=document.createElement("canvas");
 document.querySelector(".MyGame").appendChild(canvas);
 canvas.height=innerHeight;
@@ -6,11 +12,10 @@ canvas.width=innerWidth
 const context= canvas.getContext('2d');
 // =======---Main function Here-------============-----
 
-let  difficulty=21;
+let  difficulty=2;
 const lightdamage=10;
-const heavydamage=20;
-const huagedamage=50;
-let playerscore=0
+const heavydamage=25;
+let playerscore=0;
 const form =document.querySelector("form");
 const scoreboard=document.querySelector(".scoreboard");
 document.querySelector(".play-key").addEventListener("click",(e)=>{
@@ -36,36 +41,49 @@ document.querySelector(".play-key").addEventListener("click",(e)=>{
      };
 })
 
-//===--creating endscreen div and play-again==---=
-const gameoverloader=()=>{
-const gameoverbanner=document.createElement("div");
-const gameoverbutton=document.createElement("button");
-const highscore= document.createElement("div");
+/**
+ * Creates an end screen with the player's score, a high score, and a play-again button.
+ * If the player's score is higher than the previous high score, it updates the high score in local storage.
+ */
+const gameoverloader = () => {
+  // Create end screen elements
+  const gameoverBanner = document.createElement("div");
+  const gameoverButton = document.createElement("button");
+  const highscore = document.createElement("div");
 
-highscore.innerHTML=`High-Score:${
-     localStorage.getItem("highscore")
-     ?localStorage.getItem("highscore")
-     :playerscore}`;
+  // Get the previous high score from local storage
+  const oldHighScore = localStorage.getItem("highscore");
 
-const oldhighscore= 
-localStorage.getItem("highscore")&&localStorage.getItem("highscore")
-if(oldhighscore<playerscore){
-     localStorage.setItem("highscore", playerscore);
-       //===--updating playerscore in scoreboard in html--==
-       scoreboard.innerHTML=`Score:${playerscore}`
-}
-//==-=-adding text to playagain button==---
-gameoverbutton.innerHTML='Play-Again';
-gameoverbanner.appendChild(highscore);
-gameoverbanner.appendChild(gameoverbutton); 
+  // Get the current score from local storage
+  const currentScore = localStorage.getItem("playerscore");
 
-//==---makeing reload on clicking playagain button==--
-gameoverbutton.onclick=()=>{
-     window.location.reload();
-}
-gameoverbanner.classList.add("Game-Over");
-document.querySelector("body").appendChild(gameoverbanner);
-}
+  // Calculate the new high score
+  const newHighScore = Math.max(oldHighScore, currentScore);
+
+  // Update the high score in local storage
+  localStorage.setItem("highscore", newHighScore);
+
+  // Set the high score text
+  highscore.innerHTML = `High Score: ${newHighScore}`;
+
+  // Set the text for the play-again button
+  gameoverButton.innerHTML = 'Play Again';
+
+  // Reload the page when the play-again button is clicked
+  gameoverButton.onclick = () => {
+    window.location.reload();
+  };
+
+  // Add elements to the end screen div
+  gameoverBanner.appendChild(highscore);
+  gameoverBanner.appendChild(gameoverButton);
+
+  // Add class to the end screen div
+  gameoverBanner.classList.add("Game-Over");
+
+  // Append the end screen div to the body of the HTML document
+  document.querySelector("body").appendChild(gameoverBanner);
+};
 
 //====-- fixed player position----===
 const playerposition={
@@ -131,11 +149,11 @@ class Weapon{
 };
 //====--here hugeweapon class--====
 class HuageWeapon{
-     constructor(x,y,damage){
+     constructor(x,y,){
           this.x=x;
           this.y=y;
           this.color="green";
-          this.damage=damage;
+          
      };
      draw(){
           context.beginPath();
@@ -148,34 +166,49 @@ class HuageWeapon{
          this.x += 30
      }
 };
-//====================--here enemy class----========-----===---=--=----===
-class Enemy{
-     constructor(x,y,redius,color,velocity){
-          this.x=x;
-          this.y=y;
-          this.redius=redius;
-          this.color=color;
-          this.velocity=velocity;
-     };
-     draw(){
+class Enemy {
+     /**
+      * Creates a new Enemy object.
+      * @param {number} x - The x-coordinate of the enemy's position.
+      * @param {number} y - The y-coordinate of the enemy's position.
+      * @param {number} radius - The size of the enemy.
+      * @param {string} color - The color of the enemy.
+      * @param {object} velocity - The velocity of the enemy, represented as an object with x and y components.
+      */
+     constructor(x, y, radius, color, velocity) {
+          this.x = x;
+          this.y = y;
+          this.radius = radius;
+          this.color = color;
+          this.velocity = velocity;
+     }
+
+     /**
+      * Draws the enemy on the canvas.
+      */
+     draw() {
           context.beginPath();
           context.arc(
                this.x,
                this.y,
-               this.redius,
-               (Math.PI/180)*0,
-               (Math.PI/180)*360,
-               false,
-          )
-          context.fillStyle=this.color;
+               this.radius,
+               0,
+               Math.PI * 2,
+               false
+          );
+          context.fillStyle = this.color;
           context.fill();
-     };
-     update(){
-          this.draw();
-         (this.x+= this.velocity.x),
-          (this.y+= this .velocity.y) 
      }
-};
+
+     /**
+      * Updates the position of the enemy based on its velocity.
+      */
+     update() {
+          this.draw();
+          this.x += this.velocity.x;
+          this.y += this.velocity.y;
+     }
+}
 //===---here particals class---===
 
 class Partical{
@@ -246,8 +279,10 @@ const SpameEnemy=()=>{
 
      enmies.push(new Enemy(random.x,random.y,enmysize,enmycolor,velocity))
 }
-//====--- main logic here addevent listener for lightweapon--===
+//====--- main logic here addevent listener for lightweapon to rightclick--===
 canvas.addEventListener('click',(e)=>{
+     shootingsong.play();
+     //==-finding angle between player position and click co-ordinate
      const MyAngle= Math.atan2(
           e.clientY-canvas.height/2,
           e.clientX-canvas.width/2,          
@@ -266,13 +301,12 @@ canvas.addEventListener('click',(e)=>{
 
      ))
 })
-
-//====--- main logic here addevent listener for heavyweapon--===
+//====--- main logic here addevent listener for heavyweapon to leftclick--===
 canvas.addEventListener('contextmenu',(e)=>{
-     e.preventDefault();
      //===--rendering player score--==
      if(playerscore<=0)return;
-     //===-- decreasing playerscore using heavyweapons--==
+     heavyweapon.play();
+     //===-- decreasing playerscore  using heavyweapons--==
      playerscore -= 2;
      //---==updateing player score==--
      scoreboard.innerHTML=`Score:${playerscore}`
@@ -295,18 +329,17 @@ canvas.addEventListener('contextmenu',(e)=>{
 
      ))
 })
-//====--- main logic here-addevent listener for huageweapon--===
+//====--- main logic here-addevent listener for hugeweapon to spacebar--===
 addEventListener('keypress',(e)=>{
-
    if(e.key===" "){
      if(playerscore<20)return;
+     hugeweapon.play();
      playerscore -= 6;
   scoreboard.innerHTML=`Score:${playerscore}`
 
      huageweapons.push(new HuageWeapon(
          0,
          0, 
-         huagedamage
      ))
    }
     
@@ -361,11 +394,13 @@ function animation(){
           huageweapons.forEach((huageweapon)=>{
                const distancebetweenhuageweaponandenmies=huageweapon.x-enemy.x;
           
-               if(distancebetweenhuageweaponandenmies<=200 && distancebetweenhuageweaponandenmies>=-200){
+               if(distancebetweenhuageweaponandenmies<=200 &&
+                     distancebetweenhuageweaponandenmies>=-200){
                     //===--rendering player score--===
                     playerscore +=10;
                     scoreboard.innerHTML=`Score:${playerscore}`
                     setTimeout(()=>{
+                         killenemy.play();
                          enmies.splice(enemyIndex,1)
                        },0)
                }
@@ -379,6 +414,7 @@ function animation(){
                   //===--here end game if enemy hit player====---
                   if(Distance-player1.redius-enemy.redius<1){
                     cancelAnimationFrame(InimationId);
+                    gameover.play();
                     return gameoverloader()
                   }
      //==--here find distance between enemy and weapons-==
@@ -408,6 +444,7 @@ function animation(){
             //==-- updating playerscore in scoreboard in html===--
             scoreboard.innerHTML=`Score:${playerscore}`
         setTimeout(()=>{
+          killenemy.play();
           weapons.splice(weaponIndex,1)
           enmies.splice(enemyIndex,1)
         },0)
